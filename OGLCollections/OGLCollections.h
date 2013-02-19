2a5a044d98b2faa4bd7906a889058e46bcd77cf2
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2013-02-19 09:58:08 +0900

    Initial Commit

---
+++ OGLCollections/OGLCollections.h
@@ -0,0 +1,13 @@
+//
+//  OGLCollections.h
+//  OGLCollections
+//
+//  Created by organlounge on 2013/02/19.
+//  Copyright (c) 2013年 KAMEDAkyosuke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OGLCollections : NSObject
+
+@end
---
+++ OGLCollections/OGLCollections.m
@@ -0,0 +1,13 @@
+//
+//  OGLCollections.m
+//  OGLCollections
+//
+//  Created by organlounge on 2013/02/19.
+//  Copyright (c) 2013年 KAMEDAkyosuke. All rights reserved.
+//
+
+#import "OGLCollections.h"
+
+@implementation OGLCollections
+
+@end
